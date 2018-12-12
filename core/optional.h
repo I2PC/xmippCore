c77786cd39619ae020f65480d847c2c02575a5c5
David Strelak <davidstrelak@gmail.com>
2018-12-12 17:15:18 +0100

    adding new optional class

---
+++ core/optional.h
@@ -0,0 +1,74 @@
+/*
+ * optional.h
+ *
+ *  Created on: Dec 12, 2018
+ *      Author: david
+ */
+
+#ifndef CORE_OPTIONAL_H_
+#define CORE_OPTIONAL_H_
+
+#if __cplusplus >= 201703L
+#include <optional>
+namespace core::optional=std::optional
+#else
+
+#include <utility>
+#include <assert.h>
+
+namespace core {
+
+template<typename T>
+class optional {
+public:
+    optional() :
+            p(payload()) {
+    }
+    ;
+    optional(const T&& t) :
+            p(payload_full(t)) {
+    }
+    ;
+    
+    constexpr explicit operator bool() const {
+        return p.has_value;
+    }
+
+    constexpr bool has_value() const {
+        return p.has_value;
+    }
+
+    constexpr T value() const {
+        assert(p.has_value);
+        return (static_cast<const payload_full&>(p)).t;
+    }
+
+    
+private:
+
+    struct payload {
+        payload(bool has_value = false) :
+                has_value(has_value) {
+        }
+        ;
+        const bool has_value;
+    };
+
+    struct payload_full: public payload {
+        explicit payload_full(const T t) :
+                payload(true), t(std::move(t)) {
+        }
+        ;
+        const T t;
+
+    };
+
+    const payload p;
+
+};
+// optional
+
+}// namespace
+
+#endif 
+#endif /* CORE_OPTIONAL_H_ */
