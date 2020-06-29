2298f53062c47b1e56f620e4b504a5b2f7a7e38f
jeymendez <jeymendez@gmail.com>
2020-06-29 15:47:42 -0500

    adding and change some labels

--- core/metadata_label.h
+++ core/metadata_label.h
@@ -66,7 +66,6 @@ enum MDLabel
     MDL_ANGLE_Y,   ///< Angle between y-axis and tilt-axis (double, degrees) for untilted micrographs
     MDL_ANGLE_Y2,   ///< Angle between y-axis and tilt-axis (double, degrees) for tilted micrographs
     MDL_ANGLE_TEMPERATURE, ///< Angular temperature (double,degrees)
-    MDL_ANGULAR_GRAPHCONSISTENCE, ///< for validation to previous alignment
     MDL_APPLY_SHIFT,///<Apply shift when project the volume ,
     MDL_AVG, ///< average value (double)
     MDL_AVG_CHANGES_ORIENTATIONS, /// Average change in angular orientation (double degrees)
@@ -232,7 +231,11 @@ enum MDLabel
     MDL_FLIP, ///< Flip the image? (bool)
     MDL_FOM, ///< Figure of Merit in 0-1 range (double)
     MDL_FRAME_ID, ///< Unique id of frame inside a Movie
-    MDL_IDX, ///< Index within a list (size_t)
+	MDL_GRAPH_DISTANCE2MAX, ///< Distance to graph filtered max
+	MDL_GRAPH_DISTANCE2MAX_PREVIOUS, ///< when previous assignment validation
+	MDL_GRAPH_CC, ///< Correlation between assigned direction and graph filtered maximum
+	MDL_GRAPH_CC_PREVIOUS, ///< when previous assignment validation
+	MDL_IDX, ///< Index within a list (size_t)
     MDL_IMAGE, ///< Name of an image (std::string)
     MDL_IMAGE_COVARIANCE, ///< Name of the covariance imagee associated to this image
     MDL_IMAGE_IDX, ///< Index of an image within a list (size_t)
@@ -269,6 +272,7 @@ enum MDLabel
     MDL_MAXCC, ///< Maximum cross-correlation for the image (double)
     MDL_MAXCC_PERCENTILE, ///< Percentile of the maximum cross-correlation for the image (double)
     MDL_MAX, ///< Maximum value (double)
+	MDL_MAXCC_PREVIOUS, ///< Correlation from previous alignment
     MDL_MICROGRAPH, ///< Name of a micrograph (std::string)
     MDL_MICROGRAPH_ID, ///< Micrograph unique id for reference (MDL_ITEM_ID should be used for Micrographs list)
     MDL_MICROGRAPH_MOVIE, ///< Name of a movie (std::string)
@@ -364,6 +368,7 @@ enum MDLabel
     MDL_REF, ///< Class to which the image belongs (int)
     MDL_REF2, ///< Store a second class (int)
     MDL_REFMD, ///< Name of Metadata file for all references(string)
+	MDL_ASSIGNED_DIR_REF_CC, ///< correlation of references assigned by two methods
 
     MDL_RESOLUTION_DPR, ///<differential phase residual (double)
     MDL_RESOLUTION_ERRORL2, ///<Error in l2 (double)
@@ -1325,9 +1330,9 @@ private:
         MDL::addLabel(MDL_ANGLE_Y, LABEL_DOUBLE, "angleY");
         MDL::addLabel(MDL_ANGLE_Y2, LABEL_DOUBLE, "angleY2");
         MDL::addLabel(MDL_ANGLE_TEMPERATURE, LABEL_DOUBLE, "angleTemp");
-        MDL::addLabel(MDL_ANGULAR_GRAPHCONSISTENCE, LABEL_DOUBLE, "GraphConsistenceValidation");
 
         MDL::addLabel(MDL_APPLY_SHIFT, LABEL_BOOL, "applyShift");
+        MDL::addLabel(MDL_ASSIGNED_DIR_REF_CC, LABEL_DOUBLE, "assignedDirRefCC");
         MDL::addLabel(MDL_AVG, LABEL_DOUBLE, "avg");
         MDL::addLabel(MDL_AVG_CHANGES_ORIENTATIONS, LABEL_DOUBLE, "avgChanOrient");
         MDL::addLabel(MDL_AVG_CHANGES_OFFSETS, LABEL_DOUBLE, "avgChanOffset");
@@ -1562,6 +1567,11 @@ private:
         MDL::addLabel(MDL_FOM, LABEL_DOUBLE, "fom");
         MDL::addLabel(MDL_FRAME_ID, LABEL_SIZET, "frameId");
 
+        MDL::addLabel(MDL_GRAPH_DISTANCE2MAX, LABEL_DOUBLE, "distance2MaxGraph");
+        MDL::addLabel(MDL_GRAPH_DISTANCE2MAX_PREVIOUS, LABEL_DOUBLE, "distance2MaxGraphPrevious");
+        MDL::addLabel(MDL_GRAPH_CC, LABEL_DOUBLE, "graphCC");
+        MDL::addLabel(MDL_GRAPH_CC_PREVIOUS, LABEL_DOUBLE, "graphCCPrevious");
+
         MDL::addLabel(MDL_IDX, LABEL_SIZET, "index");
         MDL::addLabel(MDL_IMAGE1, LABEL_STRING, "image1", TAGLABEL_IMAGE);
         MDL::addLabel(MDL_IMAGE2, LABEL_STRING, "image2", TAGLABEL_IMAGE);
@@ -1616,6 +1626,7 @@ private:
         MDL::addLabel(MDL_MAXCC, LABEL_DOUBLE, "maxCC");
         MDL::addLabel(MDL_MAXCC_PERCENTILE, LABEL_DOUBLE, "maxCCPerc");
         MDL::addLabel(MDL_MAX, LABEL_DOUBLE, "max");
+        MDL::addLabel(MDL_MAXCC_PREVIOUS, LABEL_DOUBLE, "maxCCprevious");
         MDL::addLabel(MDL_MICROGRAPH_ID, LABEL_SIZET, "micrographId");
         MDL::addLabel(MDL_MICROGRAPH, LABEL_STRING, "micrograph", TAGLABEL_MICROGRAPH);
         MDL::addLabel(MDL_MICROGRAPH_MOVIE_ID, LABEL_SIZET, "micrographMovieId");
