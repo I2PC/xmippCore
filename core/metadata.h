cea39c054629d81c322348551e98cd879fae1092
David Strelak <davidstrelak@gmail.com>
2020-08-10 16:07:16 +0200

    cleaning metadata includes

--- core/metadata.h
+++ core/metadata.h
@@ -26,22 +26,13 @@
 #ifndef CORE_METADATA_H
 #define CORE_METADATA_H
 
-#include <map>
-#include <vector>
-#include <iostream>
-#include <iterator>
-#include <sstream>
-#include <time.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <sstream>
-#include <strings.h>
 #include <regex.h>
-#include "xmipp_funcs.h"
-#include "xmipp_strings.h"
 #include "metadata_sql.h"
 #include "xmipp_filename.h"
 #include "xmipp_error.h"
+#include "metadata_label.h"
+
+class MetaData;
 
 /** @defgroup MetaData Metadata Stuff
  * @ingroup DataLibrary
