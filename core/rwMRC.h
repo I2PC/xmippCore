89eb04d8a05a24022294ab3d049b8d555c701da1
Martin_Horacek <468922@mail.muni.cz>
2020-09-22 17:43:15 +0200

    Added batched read for MRC and SPIDER; optimized read for all formats when types are same

--- core/rwMRC.h
+++ core/rwMRC.h
@@ -16,7 +16,8 @@
 /** MRC Reader
   * @ingroup MRC
 */
-int readMRC(size_t select_img, bool isStack=false);
+int readMRC(size_t select_img, bool isStack = false);
+int readMRC(size_t start_img, size_t batch_size, bool isStack = false);
 
 /** MRC Writer
   * @ingroup MRC
