1da0ea7b9df0b6aa00723aac63d6570e1d6c8bec
MartinSalinas98 <ssalinasmartin@gmail.com>
2023-12-01 11:32:50 +0100

    Code smell

--- core/args.cpp
+++ core/args.cpp
@@ -220,8 +220,10 @@ Matrix1D<double> getVectorParameter(FILE *fh, const char *param, int dim)
     char *copy = NULL;
     Matrix1D<double> retval;
     if (!generateCommandLine(fh, param, argcp, argvp, copy))
+    {
         if (dim != -1)
             REPORT_ERROR(ERR_ARG_MISSING, param);
+    }
     else
     {
         retval = getVectorParameter(argcp, (const char **)argvp, ((std::string)"-" + param).c_str(), dim);
